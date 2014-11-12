a1990517e3e34c616138303a4d391a6da7adef32
naoyoshi <naoyoshi@www4090uo.sakura.ne.jp>
2014-11-13 03:03:07 +0900

    run ok

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ export HUBOT_SLACK_TOKEN=15QvMdcyjsS5IdZP4c1qVtkf
 export HUBOT_SLACK_TEAM=luckystars
 export HUBOT_SLACK_BOTNAME=slackbot
 
-forever start -c coffee bin/hubot --adapter slack
+PROJECT_ROOT=$HOME/deploy/lucky_bot
+COFFEE_BIN=$PROJECT_ROOT/node_modules/hubot/node_modules/coffee-script/bin/coffee
+
+cd $PROJECT_ROOT
+forever start -c $COFFEE_BIN node_modules/.bin/hubot -a slack
